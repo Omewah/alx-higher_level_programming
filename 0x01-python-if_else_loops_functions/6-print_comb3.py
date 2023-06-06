38d841359d67c0ac3a99b1740a14fe39b386dcfa
Omewah <joelomewah@ymail.com>
2023-06-06 14:43:53 -0700

    print all possible combination of 2 integers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for n1 in range(0, 10):
     for n2 in range(1, 10):
         if n2 > n1:
-            print('{}{}'.format(n1, n2), end="")
+            print("{}{}".format(n1, n2), end="")
             if int(str(n1) + str(n2)) != 89:
                 print(end=", ")
 print()
