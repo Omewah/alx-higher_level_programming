c51e63728d24b449bf70065885da0d9879c333a3
Omewah <joelomewah@ymail.com>
2023-06-08 20:29:40 +0100

    adds all argv in infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
-    result = 0
+    sum = 0
 
-    if len(sys.argv) > 1:
-        result = sum(int(i) for i in sys.argv[1:])
-        print(result)
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print(sum)
