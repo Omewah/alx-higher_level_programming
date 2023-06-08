42cc27c87fdea084a2aa999398d87903c2cb998d
Omewah <joelomewah@ymail.com>
2023-06-08 14:06:28 -0700

    calculating using operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,9 +7,13 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    operator = sys.argv[2]
+    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
+
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
-    operator = sys.argv[2]
     b = int(sys.argv{3})
 
     if operator == '+':
@@ -18,8 +22,5 @@ if __name__ == "__main__":
         print('{} {} {} = {}'.format(a, operator, b, sub(a, b)))
     elif operator == '*':
         print('{} {} {} = {}'.format(a, operator, b, mul(a, b)))
-    elif operator == '/':
-        print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
     else:
-        print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
+        print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
