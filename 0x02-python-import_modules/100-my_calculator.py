3e0b23ddbb9f192d9628f37e5586987d72f34e33
Omewah <joelomewah@ymail.com>
2023-06-08 14:10:00 -0700

    calculating using operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operator = sys.argv[2]
-    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
+    opr = sys.argv[2]
+    if opr != '+' and opr != '-' and opr != '*' and opr != '/':
         print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
 
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv{3})
 
-    if operator == '+':
-        print('{} {} {} = {}'.format(a, operator, b, add(a, b)))
-    elif operator == '-':
-        print('{} {} {} = {}'.format(a, operator, b, sub(a, b)))
-    elif operator == '*':
-        print('{} {} {} = {}'.format(a, operator, b, mul(a, b)))
+    if opr == '+':
+        print('{} {} {} = {}'.format(a, opr, b, add(a, b)))
+    elif opr == '-':
+        print('{} {} {} = {}'.format(a, opr, b, sub(a, b)))
+    elif opr == '*':
+        print('{} {} {} = {}'.format(a, opr, b, mul(a, b)))
     else:
-        print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
+        print('{} {} {} = {}'.format(a, opr, b, div(a, b)))
