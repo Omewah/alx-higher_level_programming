6d98bd49fffc51e01c70718b5d16683141a34e0c
Omewah <joelomewah@ymail.com>
2023-06-08 21:53:48 +0100

    calculating by using operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,8 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) != 3:
+    argn = len(sys.argv) - 1
+    if argn != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
