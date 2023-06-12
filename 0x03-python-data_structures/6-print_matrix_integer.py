ff2ee9e2489d6fe39d7d7ad5fec550f02558b196
Omewah <joelomewah@ymail.com>
2023-06-12 10:06:57 +0100

    prints the matrix if integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for integer in row:
-            print("{:d}".format(integer), end=" ")
+    for integer in range(len(matrix)):
+        for row in range(len(matrix[integer])):
+            if row != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[integer][row]), end='')
         print()
