c0de0047236efb63267ffaef9eea37706f2c78e8
Omewah <joelomewah@ymail.com>
2023-06-12 04:14:54 -0700

    check if divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    div_by_2 = []
-    
-    for integer in range(len(my_list)):
-        if my_list[interger] % 2 == 0:
-            div_by_2.append(True)
-        else:
-            div_by_2.append(False)
+    check_div = []
 
-    return (div_by_2)
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            check_div.append(True)
+        else:
+            check_div.append(False)
+
+    return (check_div)
