2efe51bdb37c741500634468e4935620be59012d
Omewah <joelomewah@ymail.com>
2023-06-12 12:17:58 +0100

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     can_div = []
 
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
+    for integer in range(len(my_list)):
+        if my_list[integer] % 2 == 0:
             can_div.append(True)
         else:
             can_div.append(False)
