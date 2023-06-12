61282756fdb7e80f9c708ee82e954039157d2e0b
Omewah <joelomewah@ymail.com>
2023-06-12 09:53:05 +0100

    prints the matrix if integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for integer in row:
+            print("{:d}".format(integer), end=' ')
+        print()
