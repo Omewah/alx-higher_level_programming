0448e310b2884767c2c95b73e9e96c232228a5a2
Omewah <joelomewah@ymail.com>
2023-06-12 11:17:30 +0100

    prints the max int in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_int = my_list[0]
+    for integer in my_list:
+        if integer > max_int:
+            max_int = integer
+
+    return max_int
