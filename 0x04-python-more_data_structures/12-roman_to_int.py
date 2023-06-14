00fb77f83ee2e8baf834beb9ba1157454294dadd
Omewah <joelomewah@ymail.com>
2023-06-14 20:34:23 +0100

    coverts roman numerals to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or if not isinstance(roman_string, str):
         return 0
 
     roman_val =
     {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(roman_val.keys())
 
-    convert = 0
-    p_val = 0
+    num = 0
+    last_rom = 0
+    list_num = [0]
 
-    for char in reversed(roman_string):
-        val = roman_val.get(char, 0)
+    for char in roman_string:
+        for rom_num in list_keys:
+            if rom_num == char:
+                if roman_val.get(char) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [roman_val.get(char)]
+                else:
+                    list_num.append(roman_val.get(char))
 
-        if val < p_val:
-            convert -= val
-        else:
-            convert += val
+                last_rom = roman_val.get(char)
 
-        p_val = val
+    num += to_subtract(list_num)
 
-    return convert
+    return (num)
