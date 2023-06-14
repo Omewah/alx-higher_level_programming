4208445cfb129e1a9ff0492102979b5118c8b276
Omewah <joelomewah@ymail.com>
2023-06-14 20:36:08 +0100

    coverts roman numerals to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,28 +11,30 @@ def to_subtract(list_num):
 
 
 def roman_to_int(roman_string):
-    if not roman_string or if not isinstance(roman_string, str):
+    if not roman_string:
         return 0
 
-    roman_val =
-    {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(roman_val.keys())
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
     num = 0
     last_rom = 0
     list_num = [0]
 
-    for char in roman_string:
-        for rom_num in list_keys:
-            if rom_num == char:
-                if roman_val.get(char) <= last_rom:
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
                     num += to_subtract(list_num)
-                    list_num = [roman_val.get(char)]
+                    list_num = [rom_n.get(ch)]
                 else:
-                    list_num.append(roman_val.get(char))
+                    list_num.append(rom_n.get(ch))
 
-                last_rom = roman_val.get(char)
+                last_rom = rom_n.get(ch)
 
     num += to_subtract(list_num)
 
-    return (num)
+    return (num)01~
