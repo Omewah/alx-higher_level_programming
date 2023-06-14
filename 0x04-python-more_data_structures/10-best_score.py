6c23092560f8007d56b8941519125578925bca8b
Omewah <joelomewah@ymail.com>
2023-06-14 20:04:50 +0100

    return the best scopre

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    high_score = None
-    high_key = None
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    high_key = list(a_dictionary.keys())[0]
+    high_score = a_dictionary[high_key]
 
     for key, value in a_dictionary.items():
         if high_score is None or value > high_score:
