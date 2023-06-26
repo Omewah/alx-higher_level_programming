280b776fcc99fa55dddd72a84bc79bdf3537f7a2
Omewah <joelomewah@ymail.com>
2023-06-26 21:37:19 +0100

    magic calculations

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception("Too Far")
             else:
-                calc = calc + (a ** b) / i
+                calc = calc + (a**b) / i
         except Exception:
             calc = b + a
             break
