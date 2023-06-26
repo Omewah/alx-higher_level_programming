14c4032838c59276c37d4a2a5d806cb8037e729c
Omewah <joelomewah@ymail.com>
2023-06-26 21:05:48 +0100

    functions divides 2 elements with exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,21 +2,18 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     last_list = []
-    try:
-        for i in range(list_length):
-            try:
-                E1 = my_list_1[i]
-                E2 = my_list_2[i]
-                result = E1 / E2
-            except (ValueError, TypeError):
-                print("wrong type")
-                result = 0
-            except ZeroDivisionError:
-                print("division by 0")
-                result = 0
-            except IndexError:
-                print("out of range")
-                result = 0
-            finally:
-                last_list.append(result)
-            return last_list
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except (ValueError, TypeError):
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            last_list.append(result)
+    return last_list
