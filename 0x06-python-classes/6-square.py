ffd3b9b3f7cad7c2d7aeb4fb22a602246f89940a
Omewah <joelomewah@ymail.com>
2023-06-27 20:05:00 +0100

    prints the square coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,16 +6,14 @@
 class Square:
     """the code to represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """initialize the square.
         Args:
             size (int): the size of the square.
+            position (int, int): the coordinate of the square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+        self.position = position
 
     @property
     def size(self):
@@ -30,6 +28,20 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """the current location of the square"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """prints the area of a square"""
