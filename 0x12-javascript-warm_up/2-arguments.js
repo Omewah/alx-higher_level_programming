32fcf494a39f9f3e5c4a4a154db995d8206bf545
Omewah <joleomewah@ymail.com>
2023-09-11 14:23:28 -0700

    prints messages for args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
 
-const counter = process.argv.length;
-console.log(counter === 2? 'No argument' : counter === 3? 'Argument found' : 'Arguments found');
+const count = process.argv.length;
+console.log(count === 2? 'No argument' : count === 3? 'Argument found' : 'Arguments found');
