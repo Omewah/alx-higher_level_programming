b9e8fbd286f9ec5a6e13abd9ec654d33ef15371e
Omewah <joleomewah@ymail.com>
2023-09-11 14:51:59 -0700

    a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,13 @@
 // The first argument is the size of the square
 // If the first argument can’t be converted to an integer, print “Missing size”
 
-const size = Math.floor(Numbre(process.argv[2]));
+const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let a = 0; a < size; a++) {
-        let row = '';
-        for (let b = 0; b < size; b++) row += 'X';
-        console.log('row');
-    }
+  for (let a = 0; a < size; a++) {
+    let row = '';
+    for (let b = 0; b < size; b++) row += 'X';
+    console.log('row');
+  }
 }
