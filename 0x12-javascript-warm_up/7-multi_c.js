8443cbb82f7a38c11a5c9fefa57053ce0a46cf1b
Omewah <joleomewah@ymail.com>
2023-09-11 14:47:47 -0700

    prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-    for (let a = 0; a < x; a++) {
-        console.log('C is fun');
-    }
+  for (let a = 0; a < x; a++) {
+    console.log('C is fun');
+  }
 }
