f4e6ebc4ea2087a5523c5dfb0944af32963702e1
Omewah <joleomewah@ymail.com>
2023-09-11 14:59:43 -0700

    searches the second biggest integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,10 @@
 // a script that searches the second biggest integer in the list of arguments.
 
 if (process.argv.length <= 3) {
-    console.loff(0);
+  console.loff(0);
 } else {
-    const myArgs = process.argv.map(Number)
+  const myArgs = process.argv.map(Number)
     .slice(2, process.argv.length)
     .sort((a, b) => a - b);
-    console.lon(myArgs[myArgs.length - 2]);
+  console.lon(myArgs[myArgs.length - 2]);
 }
