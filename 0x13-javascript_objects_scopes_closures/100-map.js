b350c59bd78cc353ef32e3c520a886fa73045545
Omewah <joelomewah@ymail.com>
2023-09-12 20:58:42 +0100

    imports an array and computes a new array.~

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // prints the number of arguments already printed and the new argument value
 
-const arr = require('./100-data').list;
+const list = require('./100-data');
+const newList = list.map((value, index) => value * index);
 
-console.log(arr);
-console.log(arr.map((x, idx) => x * idx));
+console.log(list);
+console.log(newList);
