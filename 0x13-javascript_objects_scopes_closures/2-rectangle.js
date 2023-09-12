af030e482c4da4876f4a20fd72b526d31ecad1f7
Omewah <joelomewah@ymail.com>
2023-09-12 19:27:57 +0100

    defines a class with instance attributes

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
